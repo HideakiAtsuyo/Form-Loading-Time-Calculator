40f162bd3a65f23fd36f811666d2c8d62101ba4e
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-04-03 15:59:20 +0200

    Upload Lmao Not a joke!

---
+++ WindowsFormsApp1/FuckIt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public static class FuckIt
+    {
+        public static Stopwatch sw = new Stopwatch();
+        public static void Stop()
+        {
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",ts.Hours, ts.Minutes, ts.Seconds,ts.Milliseconds / 10);
+            MessageBox.Show("RunTime " + elapsedTime);
+        }
+    }
+}
