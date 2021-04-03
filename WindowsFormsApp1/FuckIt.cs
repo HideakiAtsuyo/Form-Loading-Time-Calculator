9a5f5f902bed9f72d2f80835f3b031ebdcbbe4fb
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-04-03 16:02:15 +0200

    Update FuckIt.cs

--- WindowsFormsApp1/FuckIt.cs
+++ WindowsFormsApp1/FuckIt.cs
@@ -12,6 +12,7 @@ namespace WindowsFormsApp1
             sw.Stop();
             TimeSpan ts = sw.Elapsed;
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",ts.Hours, ts.Minutes, ts.Seconds,ts.Milliseconds / 10);
+            //string elapsedTime = String.Format("{0:00} ms",ts.Milliseconds / 10); //Only MS
             MessageBox.Show("RunTime " + elapsedTime);
         }
     }
